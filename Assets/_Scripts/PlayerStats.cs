66a0f32aa1c95b538c9c4842d42719bfc51310e7
joyboukaram <joy_boukaram@hotmail.com>
2019-04-08 12:05:31 +0300

    2

---
+++ Assets/_Scripts/PlayerStats.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int health;
+    public GameObject HUD;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 6;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void TakeDamage()
+    {
+        if (health > 0)
+        {
+            health -= 1;
+            HUD.GetComponent<HUDController>().UpdateHealth(health);
+        }
+        if(health == 0)
+        {
+            SceneManager.LoadScene("GameOver");
+            return;
+        }
+    }
+
+
+
+}
